e62a374a41c293ea16115057801ea9b74b4134c1
Rytis Simanavicius <rytucis.qua@gmail.com>
2020-08-20 10:00:41 +0300

    POJO comments for Javadoc

--- src/main/java/lt/viko/eif/saitynas_final_project/objects/Genre.java
+++ src/main/java/lt/viko/eif/saitynas_final_project/objects/Genre.java
@@ -1,30 +1,59 @@
 package lt.viko.eif.saitynas_final_project.objects;
 
+/**
+ * Class for objects representing movie genres.
+ * @author Rytis Simanavicius
+ *
+ */
 public class Genre {
 	private int id;
 	private String name;
 	
+	/**
+	 * Main constructor, sets all attributes of an object.
+	 * @param id
+	 * @param name
+	 */
 	public Genre(int id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 	
+	/**
+	 * Default constructor.
+	 */
 	public Genre() {
 		
 	}
 
+	/**
+	 * Returns genre id.
+	 * @return
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * Sets genre id.
+	 * @param id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	/**
+	 * Returns genre name.
+	 * @return
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Sets genre name.
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
